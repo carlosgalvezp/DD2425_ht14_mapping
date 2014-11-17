c43825baade7ecb2d54b5a5dc746e31d828e2622
ras <ras@RAS26.(none)>
2014-11-17 17:50:02 +0100

    working

--- include/robot_maps/map_handler.h
+++ include/robot_maps/map_handler.h
@@ -59,9 +59,9 @@ public:
        // updateOccupiedAreaLongSensor(dist_back_large_range, false);
 
         updateOccupiedAreaShortSensor(d_right_front, true, true);
-      //  updateOccupiedAreaShortSensor(d_right_back, true, false);
-      //  updateOccupiedAreaShortSensor(d_left_front, false, true);
-      //  updateOccupiedAreaShortSensor(d_left_back, false, false);
+        updateOccupiedAreaShortSensor(d_right_back, true, false);
+        updateOccupiedAreaShortSensor(d_left_front, false, true);
+        updateOccupiedAreaShortSensor(d_left_back, false, false);
 
         std::vector<std::string> names = {"robot_x_pos", "robot_y_pos", "robot_angle"};
         std::vector<double> values = {robot_x_pos_, robot_y_pos_, robot_angle_};
